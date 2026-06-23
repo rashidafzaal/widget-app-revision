f1b08869f4bec6eed2511c8739d9d42c2eb84421
Rashid Afzaal <rashid.afzaal@fiveriverstech.com>
2026-06-23 13:39:55 +0500

    Persist counter across app restarts using expo-file-system on iOS
    1. Added ios.entitlements to app.json for the same App Group used by CounterRepository
    2. Added a refreshWidget() native function in CounterModule.swift that calls WidgetCenter.reloadAllTimelines()

--- modules/counter/ios/CounterModule.swift
+++ modules/counter/ios/CounterModule.swift
@@ -1,7 +1,14 @@
 import ExpoModulesCore
+import WidgetKit
 
 public class CounterModule: Module {
   public func definition() -> ModuleDefinition {
     Name("Counter")
+
+    Function("refreshWidget") {
+      if #available(iOS 14.0, *) {
+        WidgetCenter.shared.reloadAllTimelines()
+      }
+    }
   }
 }
