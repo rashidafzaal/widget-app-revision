02b28835a03f778505723b8ec81a44b9e4c18383
Rashid Afzaal <rashid.afzaal@fiveriverstech.com>
2026-06-23 13:23:41 +0500

    Generate the Android widget through config
    1. Added CounterWidget.kt, layout, and widget info XML inside modules/counter/android/widget
    2. Added plugins/withAndroidWidget.js, an Expo config plugin that copies these files into the generated android/ project and injects the <receiver> entry into AndroidManifest.xml
    3. Registered the plugin in app.json
    4. Added refreshWidget() native function in CounterModule.kt that sends an update to AppWidgetManager

--- modules/counter/android/src/main/java/expo/modules/counter/CounterModule.kt
+++ modules/counter/android/src/main/java/expo/modules/counter/CounterModule.kt
@@ -1,10 +1,27 @@
 package expo.modules.counter
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Context
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class CounterModule : Module() {
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exception("React context is not available")
+
   override fun definition() = ModuleDefinition {
     Name("Counter")
+
+    Function("refreshWidget") {
+      val manager = AppWidgetManager.getInstance(context)
+      val componentName = ComponentName(context, "com.rashidafzaal1718.WidgetAppRevision.CounterWidget")
+      val ids = manager.getAppWidgetIds(componentName)
+
+      val intent = android.content.Intent(context, Class.forName("com.rashidafzaal1718.WidgetAppRevision.CounterWidget"))
+      intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+      context.sendBroadcast(intent)
+    }
   }
 }
